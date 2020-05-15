4b48542aec5046454fec9c8c9d4bd9751f5809fc
Yijie Xu <YIJIE.XU.10@UCL.AC.UK>
2020-05-15 08:22:17 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -16,10 +16,9 @@ from rdkit.Chem import AllChem
 
 REPLAY_BUFFER_CAPACITY = hyp.replay_buffer_size
 
-
- """
- Single Objective Optimization Molecule Class
- """
+"""
+Single Objective Optimization Molecule Class
+"""
 class QEDRewardMolecule(Molecule):
     """The molecule whose reward is the QED."""
 
