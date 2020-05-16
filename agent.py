2f5508058bd1e2032fdbdfccc0e6e16173f39899
Yijie Xu <YIJIE.XU.10@UCL.AC.UK>
2020-05-16 17:05:12 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -84,7 +84,8 @@ class MultiObjectiveRewardMolecule(Molecule):
       **kwargs: The keyword arguments passed to the parent class.
     """
     super(MultiObjectiveRewardMolecule, self).__init__(**kwargs)
-    target_molecule = Chem.MolFromSmiles(target_molecule)
+    #Replaced chemMolFromSmiles Argument from TargetMolecule to 'c1ccccc1'
+    target_molecule = Chem.MolFromSmiles('c1ccccc1')
     self._target_mol_fingerprint = self.get_fingerprint(target_molecule)
     self._sim_weight = similarity_weight
     self._discount_factor = discount_factor
